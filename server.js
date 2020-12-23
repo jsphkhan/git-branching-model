1285f0bd396164d85cdf2ebca4750e98cc3bb58a
Joseph Khan <joseph.khan@altayyargroup.com>
2020-12-23 13:44:59 +0400

    added root route

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get('/ping', (req, res) => {
     res.send('pong');
 });
 
+app.get('/', (req, res) => {
+    res.send('Hello there');
+});
+
 app.listen(3000, () => {
     console.log('server started at 3000');
 });
